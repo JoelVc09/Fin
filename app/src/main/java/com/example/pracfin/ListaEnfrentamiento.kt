d16b1b8cde5efb2d4ad9d09f404f5e944abe29c6
joelvc09 <joelvch09@gmail.com>
2024-07-13 00:31:07 -0500

    Primeras conecciones

---
+++ app/src/main/java/com/example/pracfin/ListaEnfrentamiento.kt
@@ -0,0 +1,20 @@
+package com.example.pracfin
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ListaEnfrentamiento : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_lista_enfrentamiento)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
--- app/src/main/java/com/example/pracfin/RegistroDeEquipos.kt
+++ app/src/main/java/com/example/pracfin/RegistroDeEquipos.kt
@@ -1,6 +1,8 @@
 package com.example.pracfin
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,17 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val btnsiguiente: Button = findViewById(R.id.btnSiguiente)
+
+        btnsiguiente.setOnClickListener {
+
+            val intent = Intent(this, RegistroEnfrentamiento::class.java)
+            startActivity(intent)
+
+
+        }
+
+
     }
 }
